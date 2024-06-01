d607dc7f93179b29942a25346cfb19af185cf2b3
DylanCoon99 <dylcoon99@gmail.com>
2024-05-31 23:05:54 -0400

    established main function logic

--- wc.py
+++ wc.py
@@ -27,7 +27,9 @@ def count_bytes(file):
 def main():
 
 	args = sys.argv[1:]
-	print(args)
+	len_args = len(args)
+	if len_args == 0: return
+
 	flags = {"-w": 0, "-l": 0, "-c": 0, "-m": 0}
 
 	flags["-w"] = 1 if "-w" in args else 0
@@ -36,6 +38,26 @@ def main():
 	flags["-m"] = 1 if "-b" in args else 0
 
 	# if no flags are present --> count everything
+
+	# file variable
+	file = args[-1]
+	# going to need to address file not found error as well as file not supplied error
+	with open(file) as f:
+		file = f.read()
+	return_values = {"word_count": 0, "line_count": 0, "char_count": 0, "byte_count": 0}
+
+
+	if flags["-w"]:
+		return_values["word_count"] = count_words(file)
+	if flags["-l"]:
+		return_values["line_count"] = count_lines(file)
+	if flags["-m"]:
+		return_values["char_count"] = count_char(file)
+	if flags["-c"]:
+		return_values["byte_count"] = count_bytes(file)
+
+
+	# going to need to format the output
 
 	return
 
